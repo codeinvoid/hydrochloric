49e32957f101c501e01a62917f79dcc860a75cc2
HoiGe <rd_idk5682@outlook.com>
2022-08-03 01:00:31 +0800

    Fix import

--- src/main/kotlin/com/pkgho/hoige/Application.kt
+++ src/main/kotlin/com/pkgho/hoige/Application.kt
@@ -3,7 +3,7 @@ package com.pkgho.hoige
 import com.pkgho.hoige.plugins.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
-import com.pkgho.plugins.*
+import com.pkgho.hoige.plugins.*
 
 fun main() {
     embeddedServer(Netty, port = 8080, host = "0.0.0.0") {
