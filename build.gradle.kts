6082479d0872d7f368115a05e87ae7c827605793
HoiGe <rd_idk5682@outlook.com>
2022-08-04 17:44:03 +0800

    FIX

--- build.gradle.kts
+++ build.gradle.kts
@@ -41,6 +41,7 @@ dependencies {
     implementation("io.ktor:ktor-server-netty-jvm:$ktor_version")
     implementation("ch.qos.logback:logback-classic:$logback_version")
     implementation("io.ktor:ktor-server-cors:$ktor_version")
+    implementation("org.litote.kmongo:kmongo:4.6.1")
     testImplementation("io.ktor:ktor-server-tests-jvm:$ktor_version")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
 }
